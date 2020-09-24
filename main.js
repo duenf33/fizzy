e23e7e230f6171a99ea3253eec9fa3bd4665721c
fernando duenas <fernando39615@aim.com>
2020-09-24 11:47:41 -0500

    first correct result

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 function fizzy(num) {
-
+  // let x = num % 3;
+  x = num % 3 === 0;
+  y = num % 5 === 0;
+  z = x === y;
+  a = 'Fizz';
+  b = 'Buzz';
+  c = "FizzBuzz";
+  if ( x && y ) {
+    return a + b;
+  } else if ( x ) {
+    return a;
+  } else if ( y ) {
+    return b;
+  } else {
+    return num;
+  }
 }
+console.log(fizzy(3));
+
+
+
 
 module.exports = {
   fizzy,
